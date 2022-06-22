85b0a8e55f330ff43e52fb2bec5221192bc6eee9
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2022-06-22 15:44:34 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [title, setTitle] = useState([
+    "남자 코트 추천",
+    "강아지 코트 추천",
+    "고양이 코트 추천",
+  ]);
+
+  const [like, setLike] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+      <div className="list">
+        <h4>
+          {title[0]} <span onClick={() => setLike(like + 1)}>👍🏻</span> {like}
+          <button
+            onClick={() => {
+              let copyTitle = [...title];
+              copyTitle[0] = "여자 코트 추천";
+              setTitle(copyTitle);
+            }}
+          >
+            제목바꾸기
+          </button>
+        </h4>
+        <p>6월 22일 발행</p>
+      </div>
+      <div className="list">
+        <h4> {title[1]} </h4>
+        <p>6월 22일 발행</p>
+      </div>
+      <div className="list">
+        <h4> {title[2]} </h4>
+        <p>6월 22일 발행</p>
+      </div>
     </div>
   );
 }
