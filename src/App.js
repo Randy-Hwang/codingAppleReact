df59df9a208c301e0b2fdb4ac2fd4e29dbbd0e13
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2022-06-27 17:26:05 +0900

    add card components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import "./App.css";
-import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import CardTitle from "./components/CardTitle";
 
 function App() {
   return (
     <div className="App">
-      <Button variant="success">Success</Button>{" "}
       <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Container className="nav_container">
+          <Navbar.Brand href="#home">Duckgu's Shoe</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
+      <div className="main_bg" />
+      <div className="goods_wrapper">
+        <div className="goods_0">
+          <CardTitle order={0} />
+        </div>
+        <div className="goods_1">
+          <CardTitle order={1} />
+        </div>
+        <div className="goods_2">
+          <CardTitle order={2} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/CardTitle.js
@@ -0,0 +1,34 @@
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import data from "../data";
+import { useState } from "react";
+
+const CardTitle = ({ order }) => {
+  let [shoes, setShoes] = useState(data);
+
+  let imgSrc;
+
+  if (order === 0) {
+    imgSrc = "https://codingapple1.github.io/shop/shoes1.jpg";
+  } else if (order === 1) {
+    imgSrc = "https://codingapple1.github.io/shop/shoes2.jpg";
+  } else if (order === 2) {
+    imgSrc = "https://codingapple1.github.io/shop/shoes3.jpg";
+  }
+
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={imgSrc} />
+      <Card.Body>
+        <Card.Title>{shoes[order].title}</Card.Title>
+        <Card.Text>
+          {shoes[order].content}
+          <br /> {shoes[order].price} ₩
+        </Card.Text>
+        <Button variant="secondary">Buy It!!</Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default CardTitle;
