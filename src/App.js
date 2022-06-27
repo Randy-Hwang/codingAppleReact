34e5b1c735552360f7702c1e67445cd44802e917
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2022-06-27 11:37:16 +0900

    start to make new page

--- src/App.js
+++ src/App.js
@@ -1,121 +1,20 @@
-import { useRef, useState } from "react";
 import "./App.css";
-
-const Modal = ({ title, setTitle, modalTitle }) => {
-  return (
-    <div className="modal">
-      <h4>{title[modalTitle]}</h4>
-      <p>Date</p>
-      <p>Text</p>
-      <button
-        onClick={() => {
-          const copyTitle = [...title];
-          copyTitle[modalTitle] = "변경된 제목";
-          setTitle(copyTitle);
-        }}
-      >
-        Edit
-      </button>
-    </div>
-  );
-};
-
-const Date = () => {
-  const today = new window.Date();
-  const year = today.getFullYear();
-  const month = today.getMonth() + 1;
-  const day = today.getDate();
-
-  return (
-    <p>
-      {year}년, {month}월, {day}일
-    </p>
-  );
-};
+import { Button, Navbar, Container, Nav } from "react-bootstrap";
 
 function App() {
-  const [title, setTitle] = useState(["남자", "여자", "고양이"]);
-  const [modal, setModal] = useState(false);
-  const [modalTitle, setModalTitle] = useState(0);
-  const [inputValue, setInputValue] = useState("");
-  const [likeNum, setLikeNum] = useState([0, 0, 0]);
-
-  const contentRef = useRef();
-
   return (
     <div className="App">
-      <div className="black-nav">
-        <h4>ReactBlog</h4>
-      </div>
-
-      {title.map((item, i) => {
-        return (
-          <div className={`list`} key={i}>
-            {/* Title, Content */}
-
-            <h4
-              onClick={() => {
-                setModal(!modal);
-                setModalTitle(i);
-              }}
-            >
-              {item}, {i + 1}번째 글
-            </h4>
-
-            {/* Like, Date */}
-            <span
-              onClick={() => {
-                const copyLike = [...likeNum];
-                copyLike[i] += 1;
-                setLikeNum(copyLike);
-              }}
-            >
-              👍🏻 {likeNum[i]}
-            </span>
-            <Date />
-
-            {/* Delete Button */}
-            <button
-              onClick={() => {
-                const copyList = [...title];
-                copyList.splice(i, 1);
-                setTitle(copyList);
-              }}
-            >
-              Delete
-            </button>
-          </div>
-        );
-      })}
-
-      {modal === true ? (
-        <Modal title={title} setTitle={setTitle} modalTitle={modalTitle} />
-      ) : null}
-
-      <input
-        ref={contentRef}
-        onChange={(e) => {
-          setInputValue(e.target.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          if (inputValue.length < 1) {
-            contentRef.current.focus();
-            return;
-          } else {
-            const copyLike = [...likeNum];
-            copyLike.unshift(0);
-            setLikeNum(copyLike);
-
-            const copyTitle = [...title];
-            copyTitle.unshift(inputValue);
-            setTitle(copyTitle);
-          }
-        }}
-      >
-        Submit
-      </button>
+      <Button variant="success">Success</Button>{" "}
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
